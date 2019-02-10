1ce1f67e112996503095c14fcb6440e052ce8672
Ryan <ryan6.williams@live.uwe.ac.uk>
2019-02-10 11:52:08 +0000

    gitignore

--- src/tabletennismanager/Game.java
+++ src/tabletennismanager/Game.java
@@ -5,24 +5,16 @@ public class Game {
     private int homeScore;
     private int awayScore;
 
-    public void Game() {
-
-    }
-
     public int getHomeScore() {
         return homeScore;
     }
-
     public void setHomeScore(int homeScore) {
         this.homeScore = homeScore;
     }
-
     public int getAwayScore() {
         return awayScore;
     }
-
     public void setAwayScore(int awayScore) {
         this.awayScore = awayScore;
     }
-
 }
